30f85e6f25c0373ab32acdcc2058dbe5b5adc818
Kelvin Kipchumba <chogekelvin90@gmail.com>
2024-11-01 11:49:39 +0300

    used find method to locate items in an object

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function superbowlWin(record){ 
+    const win = record.find(item => item.result === "W");
+    return win ? win.year : undefined;
+    /*if(record.result === "N/A"){
+            return undefined;
+    } else if (record.result ==="W"){
+        return record.year("");
+    }*/
+    //const win = record.find(item =>item.result === "w");
+    //return win.year;
+        
+    
+}
+record.find(superbowlWin);
